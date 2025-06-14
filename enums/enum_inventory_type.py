fa182e318f2f890460fd108041a7879249e3f1ac
Hamit Şimşek <117159961+lightningcell@users.noreply.github.com>
2025-06-14 17:02:09 +0300

    Fruit combination model added

--- enums/enum_inventory_type.py
+++ enums/enum_inventory_type.py
@@ -1,13 +1,33 @@
 from enum import Enum
 
 class InventoryType(Enum):
-    Fruit = "Fruit"
+    # Only Amount
     Shell = "Shell"
+    Fishbucks = "Fishbucks"
+    Fishcoins = "Fishcoins"
+    Energy = "Energy"
+    
+    # Amount and Type
+    Fruit = "Fruit"
     Look = "Look"
     Bait = "Bait"
     Rod = "Rod"
     Decoration = "Decoration"
-    Fishbucks = "Fishbucks"
-    Fishcoins = "Fishcoins"
+    
+    # Only Type
+    Fish = "Fish"
+    
+    # Special Types
     Fishbox = "Fishbox"
-    Energy = "Energy"
+    
+    def is_only_amount(self):
+        return self in {InventoryType.Shell, InventoryType.Fishbucks, InventoryType.Fishcoins, InventoryType.Energy}
+    
+    def is_amount_and_type(self):
+        return self in {InventoryType.Fruit, InventoryType.Look, InventoryType.Bait, InventoryType.Rod, InventoryType.Decoration}
+    
+    def is_only_type(self):
+        return self in {InventoryType.Fish, InventoryType.Fishbox}
+    
+    def is_special_type(self):
+        return self == InventoryType.Fishbox
